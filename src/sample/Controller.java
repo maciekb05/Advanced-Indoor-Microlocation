b7716ba6773bb5d49d2c395b15412ee7daad27a0
maciekb05 <maciej.bielech@gmail.com>
2017-12-06 16:43:01 +0100

    Added button, ad text

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,23 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.text.*;
+
 public class Controller {
+    @FXML
+    Text RSSI1;
+    @FXML
+    Text RSSI2;
+    @FXML
+    Text RSSI3;
+    @FXML
+    Text RSSI4;
+
+    @FXML
+    public void initialize() {
+        RSSI1.setText("000");
+        RSSI2.setText("000");
+        RSSI3.setText("000");
+        RSSI4.setText("000");
+    }
 }
