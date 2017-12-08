7e85fce0af6727ef464cb80badd3078585802e3d
Tomasz Pęcak <pecatoma@gmail.com>
2017-12-08 21:31:23 +0100

    Added workspace.xml to gitignore Added MapBuilder

---
+++ src/mapBuilder/MapBuilder.java
@@ -0,0 +1,16 @@
+package mapBuilder;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MapBuilder extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
