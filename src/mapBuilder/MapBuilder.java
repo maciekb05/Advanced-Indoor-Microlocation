d23817a6350c38a2a51a13fbe6c3ab3c8856bace
Tomasz Pęcak <pecatoma@gmail.com>
2017-12-09 04:02:27 +0100

    First version of MapBuilder Added Buttons, Pane, adding beacon and obstacle and setting their parametres

--- src/mapBuilder/MapBuilder.java
+++ src/mapBuilder/MapBuilder.java
@@ -1,16 +1,23 @@
 package mapBuilder;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MapBuilder extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("mapBuilder.fxml"));
+        primaryStage.setTitle("Advanced Indoor Microlocation MapBuilder");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 
-    @Override
-    public void start(Stage primaryStage) {
 
+    public static void main(String[] args) {
+        launch(args);
     }
 }
